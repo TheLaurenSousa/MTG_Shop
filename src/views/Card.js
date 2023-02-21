1dc18dbf6691f8bdafbafaa4640ebeeca0b642d6
Lauren Sousa <thelaurensousa@gmail.com>
2023-02-21 11:34:48 -0500

    Card Styling Part 2

--- src/views/Card.js
+++ src/views/Card.js
@@ -24,26 +24,26 @@ export default () => {
                 <div className='cardInfo'>
                     <div>
                         <img src={data.imageUrl} alt={data.name} className="card"/>
+                        <p id='artist'>Artist: {data.artist}</p>
                     </div>
-                    <div>
+                    <div id="cardDescription">
                         <h1>{data.name}</h1>
-                        <p>Mana Cost: {data.manaCost}</p>
-                        <p>Colors: {data.colors}</p>
-                        <p>Type: {data.type}</p>
+                        <p><span className='feature'>Mana Cost: </span>{data.manaCost}</p>
+                        <p><span className='feature'>Colors: </span>{data.colors}</p>
+                        <p><span className='feature'>Type: </span>{data.type}</p>
                         {data.subtype? 
                             <div>
-                                Subtypes: 
+                                <span className='feature'>Subtypes: </span>
                                 {data.subtypes.map((subtype, index) => {
                                     return (<p key={index}>{subtype}</p>)
                                 })}
                             </div>
                             : null
                         }
-                        <p>Rarity: {data.rarity}</p>
-                        <p>Text: {data.text}</p>
-                        <p>Power: {data.power}</p>
-                        <p>Toughness: {data.toughness}</p>
-                        <p>Artist: {data.artist}</p>
+                        <p><span className='feature'>Rarity: </span>{data.rarity}</p>
+                        <p><span className='feature'>Text: </span>{data.text}</p>
+                        <p><span className='feature'>Power: </span>{data.power}</p>
+                        <p><span className='feature'>Toughness: </span>{data.toughness}</p>
                     </div>
                 </div>
             :null}
